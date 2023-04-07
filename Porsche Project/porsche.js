a421294a2beed6b53500c37192cd4334068adb5f
Jorge Casta <jorgerochin0102@gmail.com>
2023-04-07 15:58:59 -0700

    JSON Card Data Running

--- Porsche Project/porsche.js
+++ Porsche Project/porsche.js
@@ -1,3 +1,32 @@
+let grid = document.querySelector('.col-md-4');
 
+fetch('http://localhost:3000/inventory')
+.then(res => res.json())
+.then(data => {
+    for(let value of data){
+        addElement(grid, value);
+    }
+})
 
+function addElement(appendIn, value){
+    let div = document.createElement('div');
+    div.className = "card mb-4 box-shadow";
 
+    let {image, name} = value;
+
+    div.innerHTML = `
+    <img class="card-img-top" src="${image}"
+    style="height: 225px; width: 100%; display: block;">
+    <div class="card-body" style="">
+      <p class="card-text" style="">${name}</p>
+        <div class="d-flex justify-content-between align-items-center">
+          <div class="btn-group">
+            <button type="button" class="btn btn-sm btn-outline-primary">View</button>
+            <button type="button" class="btn btn-sm btn-outline-secondary">Buy Now</button>
+          </div>
+        </div>
+    </div>
+    `
+
+    appendIn.appendChild(div);
+}
