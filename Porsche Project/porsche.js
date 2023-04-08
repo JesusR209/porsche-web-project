56b68d4d6390ff4df1b131e401f0a23cafe9724e
Jesus <Jesus092402.jr@gmail.com>
2023-04-07 18:06:16 -0700

    empty card removal

--- Porsche Project/porsche.js
+++ Porsche Project/porsche.js
@@ -10,19 +10,20 @@ fetch('http://localhost:3000/inventory')
 
 function addElement(appendIn, value){
     let div = document.createElement('div');
-    div.className = "card mb-4 box-shadow";
+    div.className = "card md-6 m-3 box-shadow";
 
     let {image, name} = value;
 
     div.innerHTML = `
+    
     <img class="card-img-top" src="${image}"
     style="height: 225px; width: 100%; display: block;">
-    <div class="card-body" style="">
-      <p class="card-text" style="">${name}</p>
+    <div class="card-body " style="">
+      <p class="card-text" style=""><i>${name}<i></p>
         <div class="d-flex justify-content-between align-items-center">
           <div class="btn-group">
             <button type="button" class="btn btn-sm btn-outline-primary">View</button>
-            <button type="button" class="btn btn-sm btn-outline-secondary">Buy Now</button>
+            <button type="button" class="btn btn-sm btn-outline-secondary"><strong>Buy Now<strong></button>
           </div>
         </div>
     </div>
@@ -30,3 +31,5 @@ function addElement(appendIn, value){
 
     appendIn.appendChild(div);
 }
+
+
